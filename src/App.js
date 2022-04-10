d3d562c1763d5c03975dd96c2d066b2be676319f
Nathan Schmidt <natepsacc@gmail.com>
2022-04-09 19:45:15 -0700

    three

--- src/App.js
+++ src/App.js
@@ -3,9 +3,15 @@ import './App.css';
 
 function App() {
 const urlone = "https://ipfs.fleek.co/ipfs/bafybeibuhucj4xd4vstpjdq6r66oieluxq6qai64fy7fkoxp3qfp6dlzhy";
-
+const urltwo = "https://ipfs.fleek.co/ipfs/bafybeid63dh22g6ljxtayse44xizagavdplylovvkhhqowi6wokdxmp5dq";
+const urlthr = "https://ipfs.fleek.co/ipfs/bafybeif2dwplp55xxqjw6d44aje2gmfnr32b3nozkbasdskrxqsezjltpe";
   return(
+<div>
 <img alt='schidtcomotors' src={urlone} id='schidtcomotors'></img>
+<img alt='schidtcomotorstwo' src={urltwo} id='schidtcomotors'></img>
+<img alt='schidtcomotorsthree' src={urlthr} id='schidtcomotors'></img>
+</div>
+
 )
 }
 
