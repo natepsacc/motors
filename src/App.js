30c55477793dda2290572a1be05e39bfd0eaf257
Nathan Schmidt <natepsacc@gmail.com>
2022-04-09 19:14:04 -0700

    first edition

--- src/App.js
+++ src/App.js
@@ -2,24 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const urlone = "https://ipfs.fleek.co/ipfs/bafybeickcaq5fgc6t2ex6ler42t372iwyv6pit2jz7cj7jslbpksa4yl7y";
+
+  return(
+<img alt='schidtcomotors' src={urlone} id='schidtcomotors'></img>
+)
 }
 
 export default App;
